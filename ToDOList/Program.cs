32306f01f7b58527dae8c7a629a4556754e7391d
Maxim Papko <papkom50@gmail.com>
2024-11-28 21:22:13 +0200

    initial commit

---
+++ ToDOList/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test message");
+        }
+    }
+}
